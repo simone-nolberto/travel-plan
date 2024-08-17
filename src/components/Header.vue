6e8ff631c1877164813ddc5805b8a25ad7aa4814
simone-nolberto <simone.nolberto@outlook.com>
2024-08-17 13:36:25 +0200

    add split structure for completed trip and planned ones

--- src/components/Header.vue
+++ src/components/Header.vue
@@ -9,11 +9,11 @@ export default {
 
 <template>
   <header>
-    <div class="navbar mx-3 d-flex flex-column">
+    <div class="navbar my-4 gap-3 mx-3 d-flex flex-column">
       <h1>Travel Plan</h1>
 
       <div class="right-buttons">
-        <ul class="d-flex gap-5">
+        <ul class="d-flex gap-5 list-unstyled">
           <li><button>Add a trip</button></li>
           <li><button>Plan a trip</button></li>
         </ul>
--- src/components/Main.vue
+++ src/components/Main.vue
@@ -9,45 +9,92 @@ export default {
 
 <template>
   <main class="container">
-    <div>
-      <div class="mb-3">
-        <label for="" class="form-label">Trip or vacation</label>
-        <input
-          type="text"
-          class="form-control"
-          name=""
-          id=""
-          aria-describedby="helpId"
-          placeholder="New York"
-        />
-        <small id="helpId" class="form-text text-muted"
-          >Type here the trip you want to find</small
-        >
-      </div>
-    </div>
+    <div class="row">
+      <div class="col">
+        <div>
+          <div class="mb-3">
+            <label for="" class="form-label">Trip or vacation</label>
+            <input
+              type="text"
+              class="form-control"
+              name=""
+              id=""
+              aria-describedby="helpId"
+              placeholder="New York"
+            />
+            <small id="helpId" class="form-text text-muted"
+              >Type here the trip you want to find</small
+            >
+          </div>
+        </div>
 
-    <div class="table-responsive rounded">
-      <table class="table table-primary">
-        <thead>
-          <tr>
-            <th scope="col">Where have you been?</th>
-            <th scope="col">When?</th>
-            <th scope="col">Actions</th>
-          </tr>
-        </thead>
-        <tbody>
-          <tr class="">
-            <td scope="row">London</td>
-            <td>19/10/98</td>
-            <td>Open</td>
-          </tr>
-          <tr class="">
-            <td scope="row">London</td>
-            <td>19/10/98</td>
-            <td>Open</td>
-          </tr>
-        </tbody>
-      </table>
+        <div class="table-responsive rounded">
+          <table class="table table-primary">
+            <thead>
+              <tr>
+                <th scope="col">Where have you been?</th>
+                <th scope="col">When?</th>
+                <th scope="col">Actions</th>
+              </tr>
+            </thead>
+            <tbody>
+              <tr class="">
+                <td scope="row">London</td>
+                <td>19/10/98</td>
+                <td>Open <i class="fa fa-eye" aria-hidden="true"></i></td>
+              </tr>
+              <tr class="">
+                <td scope="row">London</td>
+                <td>19/10/98</td>
+                <td>Open <i class="fa fa-eye" aria-hidden="true"></i></td>
+              </tr>
+            </tbody>
+          </table>
+        </div>
+      </div>
+
+      <div class="col">
+        <div>
+          <div class="mb-3">
+            <label for="" class="form-label">Plan</label>
+            <input
+              type="text"
+              class="form-control"
+              name=""
+              id=""
+              aria-describedby="helpId"
+              placeholder="New York"
+            />
+            <small id="helpId" class="form-text text-muted"
+              >Type here the plan you want to find</small
+            >
+          </div>
+        </div>
+
+        <div class="table-responsive rounded">
+          <table class="table table-success">
+            <thead>
+              <tr>
+                <th scope="col">Where would you like to go?</th>
+                <th scope="col">When?</th>
+                <th scope="col">Actions</th>
+              </tr>
+            </thead>
+            <tbody>
+              <tr class="">
+                <td scope="row">London</td>
+                <td>19/10/98</td>
+                <td>Open <i class="fa fa-eye" aria-hidden="true"></i></td>
+              </tr>
+              <tr class="">
+                <td scope="row">London</td>
+                <td>19/10/98</td>
+                <td>Open <i class="fa fa-eye" aria-hidden="true"></i></td>
+              </tr>
+            </tbody>
+          </table>
+        </div>
+      </div>
     </div>
   </main>
 </template>
